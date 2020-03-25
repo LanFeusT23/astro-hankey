07cdd00245aea081d20667493681829af5993a7b
jonathan hankey <jhankey@blizzard.com>
2020-03-25 14:37:49 -0700

    Title update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
 	<div class="relative h-full p-6 pt-24 bg-center bg-no-repeat bg-cover md:p-12 md:pt-24 lg:p-24 lg:pt-32 home">
-		<div class="text-3xl font-bold leading-none lg:text-5xl text-shadow-md">
-			Jonathan Hankey
-		</div>
-		<div class="text-xl font-bold lg:text-2xl">
-			Astrophotography
+		
+		<div class="flex justify-center mt-10">
+			<div class="text-3xl font-bold leading-none text-white uppercase lg:text-5xl text-shadow-md">
+				Jonathan Hankey
+				
+				<div class="flex justify-between text-xl font-bold uppercase home__subtitle lg:text-2xl">
+					<span>A</span>
+					<span>S</span>
+					<span>T</span>
+					<span>R</span>
+					<span>O</span>
+					<span>P</span>
+					<span>H</span>
+					<span>O</span>
+					<span>T</span>
+					<span>O</span>
+					<span>G</span>
+					<span>R</span>
+					<span>A</span>
+					<span>P</span>
+					<span>H</span>
+					<span>Y</span>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
