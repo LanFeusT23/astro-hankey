1d402fa56faa04600585060caf073ac093914520
LanFeusT23 <lanfeust23@gmail.com>
2020-03-25 20:08:55 -0700

    Temp fix for service worker refresh

--- src/components/shared/NewVersion.vue
+++ src/components/shared/NewVersion.vue
@@ -1,44 +1,41 @@
 <template>
-    <AlertNotification :showNotification="newServiceWorker" class="z-50 font-bold">
+    <AlertNotification :showNotification="true" class="container z-50 font-bold">
         <span>New version available!</span>
         <a @click="refresh" class="ml-2 font-bold text-green-600 uppercase cursor-pointer new-version__refresh-page">Refresh</a>
     </AlertNotification>
 </template>
 
 <script>
-    import { EVENTS } from "@/registerServiceWorker.js"
-    import AlertNotification from "@/components/shared/AlertNotification"
+import { EVENTS } from "@/registerServiceWorker.js"
+import AlertNotification from "@/components/shared/AlertNotification"
 
-    export default {
-        name: "NewVersion",
-        components: {
-            AlertNotification
+export default {
+    name: "NewVersion",
+    components: {
+        AlertNotification
+    },
+    data() {
+        return {
+            newServiceWorker: false
+        }
+    },
+    created() {
+        window.document.addEventListener(EVENTS.UPDATED, this.newVersion)
+    },
+    methods: {
+        newVersion(event) {
+            this.newServiceWorker = event?.detail?.registration ? true : false
         },
-        data() {
-            return {
-                newServiceWorker: false
-            }
-        },
-        created() {
-            window.document.addEventListener(EVENTS.UPDATED, this.newVersion);
-        },
-        methods: {
-            newVersion(event) {
-                this.newServiceWorker = event?.detail?.registration ? true : false;
-            },
-            refresh() {
-                if (this.newServiceWorker) {
-                    window.document.dispatchEvent(new CustomEvent(EVENTS.REFRESH));
-                } else {
-                    window.location = "/"
-                }
-            }
-        },
-        beforeDestroy() {
-            window.document.removeEventListener(
-                EVENTS.UPDATED,
-                this.newVersion
-            );
-        },
+        refresh() {
+            // if (this.newServiceWorker) {
+            //     window.document.dispatchEvent(new CustomEvent(EVENTS.REFRESH))
+            // } else {
+            window.location = "/"
+            // }
+        }
+    },
+    beforeDestroy() {
+        window.document.removeEventListener(EVENTS.UPDATED, this.newVersion)
     }
+}
 </script>
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -8,38 +8,38 @@ export const EVENTS = {
     CONTROLLER_CHANGE: "controllerchange"
 }
 
-if (process.env.NODE_ENV === "production") {
-    register(`${process.env.BASE_URL}service-worker.js`, {
-        ready() {
-            console.log("App is being served from cache by a service worker.\n" + "For more details, visit https://goo.gl/AFskqB")
-        },
-        registered() {
-            console.log("Service worker has been registered.")
-        },
-        cached() {
-            console.log("Content has been cached for offline use.")
-        },
-        updatefound() {
-            console.log("New content is downloading.")
-        },
-        updated(registration) {
-            console.log("New content is available; please refresh.")
+//if (process.env.NODE_ENV === "production") {
+register(`${process.env.BASE_URL}service-worker.js`, {
+    ready() {
+        console.log("App is being served from cache by a service worker.\n" + "For more details, visit https://goo.gl/AFskqB")
+    },
+    registered() {
+        console.log("Service worker has been registered.")
+    },
+    cached() {
+        console.log("Content has been cached for offline use.")
+    },
+    updatefound() {
+        console.log("New content is downloading.")
+    },
+    updated(registration) {
+        console.log("New content is available; please refresh.")
 
-            window.document.addEventListener(EVENTS.REFRESH, () => {
-                registration.waiting.postMessage({ action: "skipWaiting" })
+        window.document.addEventListener(EVENTS.REFRESH, () => {
+            registration.waiting.postMessage({ action: "skipWaiting" })
+        })
+
+        window.document.dispatchEvent(
+            new CustomEvent(EVENTS.UPDATED, {
+                detail: { registration }
             })
-
-            window.document.dispatchEvent(
-                new CustomEvent(EVENTS.UPDATED, {
-                    detail: { registration }
-                })
-            )
-        },
-        offline() {
-            console.log("No internet connection found. App is running in offline mode.")
-        },
-        error(error) {
-            console.error("Error during service worker registration:", error)
-        }
-    })
-}
+        )
+    },
+    offline() {
+        console.log("No internet connection found. App is running in offline mode.")
+    },
+    error(error) {
+        console.error("Error during service worker registration:", error)
+    }
+})
+//}
