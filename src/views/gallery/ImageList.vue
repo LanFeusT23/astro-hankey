20cac4f734c121851593e63dbfa0773a3c52e39a
LanFeusT23 <lanfeust23@gmail.com>
2020-03-29 10:53:52 -0700

    grid animation gallery

--- src/views/gallery/ImageList.vue
+++ src/views/gallery/ImageList.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="grid justify-center gap-4 p-4 pt-24 md:p-12 md:pt-24 lg:p-24 lg:pt-32 gallery ">
         <router-link
+            ref="thumbnail"
+            :class="{'opacity-0': hideImages }"
             v-for="post in sortedPosts"
             :to="`/gallery/${post.id}`"
             :key="post.id">
@@ -10,6 +12,7 @@
 </template>
 
 <script>
+    import { gsap } from "gsap"
     import Thumbnail from "@/components/gallery/Thumbnail"
 
     export default {
@@ -17,11 +20,53 @@
         components: {
             Thumbnail
         },
+        data() {
+            return {
+                hideImages: true
+            }
+        },
         computed: {
             sortedPosts() {
                 return [
                     ...this.$store.getters["posts/sortedPosts"],
                 ]
+            }
+        },
+        methods: {
+            animateGridIn() {
+                const thumbnails = this.$refs.thumbnail?.map(x => x.$el)
+
+                let self = this
+                gsap.fromTo(thumbnails, {
+                    scale: 0.1,
+                    y: 60,
+                    opacity: 0
+                },{
+                    scale: 1,
+                    duration: .5,
+                    opacity: 1,
+                    y: 0,
+                    ease: "power1.inOut",
+                    stagger: {
+                        amount: 0.5,
+                        from: "start"
+                    }
+                })
+            }
+        },
+        watch: {
+            sortedPosts: {
+                async handler(val) {
+                    if (val.length > 0) {
+                        await this.$nextTick() 
+                        this.animateGridIn()
+                    }
+                }, immediate: false
+            }
+        },
+        mounted() {
+            if (this.$store.state.route.from?.name === "ImagePost") {
+                this.hideImages = false
             }
         }
     }
