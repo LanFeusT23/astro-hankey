58c166dc26fe2653f6e6f30c878700ba11adf32a
LanFeusT23 <lanfeust23@gmail.com>
2020-03-29 13:33:58 -0700

    Fixed animations using transition-group

--- src/views/gallery/ImageList.vue
+++ src/views/gallery/ImageList.vue
@@ -1,29 +1,30 @@
 <template>
-    <div class="grid justify-center gap-4 p-4 pt-24 md:p-12 md:pt-24 lg:p-24 lg:pt-32 gallery ">
+    <transition-group 
+        name="animate-grid"
+        tag="div"
+        class="grid justify-center gap-4 p-4 pt-24 md:p-12 md:pt-24 lg:p-24 lg:pt-32 gallery "
+        @enter="enter"
+        v-bind:css="false">
         <router-link
             ref="thumbnail"
-            :class="{'opacity-0': hideImages }"
-            v-for="post in sortedPosts"
+            v-for="(post, index) in sortedPosts"
             :to="`/gallery/${post.id}`"
+            :data-index="index"
             :key="post.id">
             <Thumbnail v-bind="post" />
         </router-link>
-    </div>
+    </transition-group>
 </template>
 
 <script>
     import { gsap } from "gsap"
     import Thumbnail from "@/components/gallery/Thumbnail"
+    const DELAY_BETWEEN_IMAGES_IN_MS = 50
 
     export default {
         name: "ImageList",
         components: {
             Thumbnail
-        },
-        data() {
-            return {
-                hideImages: true
-            }
         },
         computed: {
             sortedPosts() {
@@ -33,40 +34,22 @@
             }
         },
         methods: {
-            animateGridIn() {
-                const thumbnails = this.$refs.thumbnail?.map(x => x.$el)
+            enter(el, done) {
+                var delay = el.dataset.index * DELAY_BETWEEN_IMAGES_IN_MS / 1000
 
-                let self = this
-                gsap.fromTo(thumbnails, {
+                gsap.fromTo(el, {
                     scale: 0.1,
-                    y: 60,
+                    x: "10rem",
                     opacity: 0
                 },{
                     scale: 1,
                     duration: .5,
                     opacity: 1,
-                    y: 0,
+                    x: 0,
+                    delay: delay,
                     ease: "power1.inOut",
-                    stagger: {
-                        amount: 0.5,
-                        from: "start"
-                    }
+                    onComplete: done
                 })
-            }
-        },
-        watch: {
-            sortedPosts: {
-                async handler(val) {
-                    if (val.length > 0) {
-                        await this.$nextTick() 
-                        this.animateGridIn()
-                    }
-                }, immediate: false
-            }
-        },
-        mounted() {
-            if (this.$store.state.route.from?.name === "ImagePost") {
-                this.hideImages = false
             }
         }
     }
