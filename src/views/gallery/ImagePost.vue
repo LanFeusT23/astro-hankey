cd2327ed2f87d46f3cd866c197a8d944a98c154d
Jonathan Hankey <lanfeust23@gmail.com>
2020-07-03 17:06:25 -0700

    testing meta

--- src/views/gallery/ImagePost.vue
+++ src/views/gallery/ImagePost.vue
@@ -46,24 +46,21 @@ export default {
                 },
                 {
                     property: "og:url",
-                    content: `https://astrohankey.space/gallery/${this.selectedPost?.id}`
-                },
-                {
-                    property: "og:description",
-                    content: ""
+                    content: "https://astrohankey.space/gallery/iss_transit_stack-2020-07-03"
+                    // content: `https://astrohankey.space/gallery/${this.selectedPost?.id}`
                 },
                 {
                     property: "og:title",
-                    content: this.selectedPost?.title
+                    content: "Jonathan Hankey Astrophotography"
                 },
                 {
                     property: "og:image",
-                    content: this.selectedPost?.thumbnailUrl
+                    content: "http://ogp.me/logo.png"
                 },
-                {
-                    property: "og:image:secure_url",
-                    content: this.selectedPost?.thumbnailUrl
-                }
+                // {
+                //     property: "og:image:secure_url",
+                //     content: this.selectedPost?.thumbnailUrl
+                // }
                 // {
                 //     property: "og:title",
                 //     content: "Jonathan Hankey Astrophotography"
